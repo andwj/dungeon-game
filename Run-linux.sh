781a3287e6ae8c61ca6be9cc4077872f1bbc9eb8
Andrew Apted <ajapted@gmail.com>
2014-10-08 23:15:13 +1100

    Run-linux script : if 'bin/darkplaces-custom' exists, run that.

--- Run-linux.sh
+++ Run-linux.sh
@@ -1,6 +1,10 @@
 #/bin/sh
-# if installed by a package manager, run that one
-if type -t darkplaces ; then
+
+# if a custom-built one exists, run it
+if [ -f bin/darkplaces-custom ] ; then
+  bin/darkplaces-custom -customgamedirname1 oga ;
+# or if installed from a package, run that one
+elif type -t darkplaces ; then
   darkplaces -customgamedirname1 oga ;
 else
   bin/darkplaces-linux-686-sdl -customgamedirname1 oga ;
