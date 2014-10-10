ff90937b9105eb7eb50dc68b6bea04065cd7a179
Andrew Apted <ajapted@gmail.com>
2014-10-10 11:36:50 +1100

    pack-it script : copy the 'Run.bat' scripts, and be more verbose about what is being done.

--- misc/pack-it.sh
+++ misc/pack-it.sh
@@ -3,21 +3,29 @@ set -e
 
 dest=oga-game-XXX
 
+echo "Packing everything to.... " $dest
+
 mkdir $dest
 
 # Binaries
 # (no actual EXEs.... yet)
-mkdir $dest/bin
+mkdir -v $dest/bin
+
+cp -v Run.bat $dest
+cp -v Run-linux.sh $dest
+chmod 755 $dest/Run-linux.sh
 
 # Data
-cp -a oga $dest/oga
+cp -av oga $dest/oga
 
 # Source code
-mkdir $dest/source
+mkdir -v $dest/source
 
-cp -a game   $dest/source/game
-cp -a client $dest/source/client
-cp -a menu   $dest/source/menu
+cp -av game   $dest/source/game
+cp -av client $dest/source/client
+cp -av menu   $dest/source/menu
+
+rm -fv $dest/*/fteqcc.log
 
 #  Documentation
 #TODO
