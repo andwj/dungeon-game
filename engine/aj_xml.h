d4d5d04bf5951e4ab97aa2310c11d8c3f03be4d1
Andrew Apted <ajapted@gmail.com>
2014-11-02 21:14:04 +1100

    Engine : began work on some custom code for parsing XML files, out of pure disgust for everything else out there....

---
+++ engine/aj_xml.h
@@ -0,0 +1,29 @@
+
+/*******************
+   AJ XML PARSER
+********************
+
+by Andrew Apted, November 2014
+
+
+The code herein is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this code.  Use at your own risk.
+
+Permission is granted to anyone to use this code for any purpose,
+including commercial applications, and to freely redistribute and
+modify it.  An acknowledgement would be nice, but is not required.
+
+
+__ABOUT__
+
+This is an incredibly simple, naive, slow, inefficent, non-conforming,
+non-validating, non-everything parser for XML files, written in C.
+If that is not what you want or need, you can find much better XML
+parsing libraries out there (mostly in C++ mind you).
+
+*/
+
+
+
+
